0df2421f8c119f22738da81af4d577cb0163a149
borenzheng <boren_zheng@uri.edu>
2019-06-20 16:42:53 -0400

    practice on Files

---
+++ ex3_Hacking/script.py
@@ -0,0 +1,52 @@
+# passwords.csv provided by Codecademy learning platform
+
+# Reading In The Passwords
+import csv
+# import os
+# os.chdir('/Users/boren/python_project/Python/ex3_Hacking')
+
+compromised_users = []
+with open('passwords.csv') as password_file:
+  password_csv = csv.DictReader(password_file)
+  for row in password_csv:
+    password_row = row
+    compromised_users.append(password_row['Username'])
+    
+with open('compromised_users.txt', 'w') as compromised_user_file:
+  for user in compromised_users:
+    compromised_user_file.write(user)
+    
+# Notifying the Boss
+import json
+
+with open('boss_message.json', 'w') as boss_message:
+  boss_message_dict ={
+    "recipient" : "The Boss",
+    "message" : "Mission Success"
+  }
+  json.dump(boss_message_dict, boss_message)
+  
+# Scrambling the Password
+with open('new_passwords.csv', 'w') as new_passwords_obj:
+  slash_null_sig = """
+ _  _     ___   __  ____             
+/ )( \   / __) /  \(_  _)            
+) \/ (  ( (_ \(  O ) )(              
+\____/   \___/ \__/ (__)             
+ _  _   __    ___  __ _  ____  ____  
+/ )( \ / _\  / __)(  / )(  __)(    \ 
+) __ (/    \( (__  )  (  ) _)  ) D ( 
+\_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+        ____  __     __   ____  _  _ 
+ ___   / ___)(  )   / _\ / ___)/ )( \
+(___)  \___ \/ (_/\/    \\___ \) __ (
+       (____/\____/\_/\_/(____/\_)(_/
+ __ _  _  _  __    __                
+(  ( \/ )( \(  )  (  )               
+/    /) \/ (/ (_/\/ (_/\             
+\_)__)\____/\____/\____/
+"""
+  new_passwords_obj.write(slash_null_sig)
+  
+  
+  
